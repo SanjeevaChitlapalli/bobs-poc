639138cbf96047c37ecd830e323012b5db265c71
Sanjeeva Chitlapalli <sanjeeva.chitlapalli@thoughtclan.com>
2024-10-14 15:19:09 +0530

    added a test api to make the call

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import useUuid from './useUuid';
 
 const App = () => {
-  const apiUrl = 'http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1'; 
+  const apiUrl = 'http://www.randomnumberapi.com/api/v1.0/uuid'; 
   const uuid = useUuid(apiUrl); 
 
   return (
